976eb975d6189b6d19079a8e26526069ee340d4c
Daniel Semac <dsemac@gmail.com>
2016-07-12 23:49:57 -0500

    removed comment

--- pokenope.js
+++ pokenope.js
@@ -1,5 +1,3 @@
-// document.body.innerHTML = document.body.innerHTML.replace(new RegExp("user1", "g"), "nobody");
-
 var x = document.getElementsByClassName("userContent");
 
 for (var i = 0, length = x.length; i < length; i ++)
